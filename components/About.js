e830810a7d8cb10b352b6ec05312f1ea378f5ac2
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2021-02-15 17:08:44 -0500

    Updated about

--- components/About.js
+++ components/About.js
@@ -10,7 +10,7 @@ import {
 
 const About = () => {
   const fullText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin tempor id eu nisl. Ut sem viverra aliquet eget sit amet tellus cras. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Turpis nunc eget lorem dolor sed viverra ipsum nunc. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Pellentesque adipiscing commodo elit at imperdiet dui. Sodales ut eu sem integer vitae justo eget magna fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada. Purus sit amet volutpat consequat mauris nunc congue nisi. Tellus elementum sagittis vitae et leo duis ut. Nisl purus in mollis nunc sed id semper risus in. Viverra nam libero justo laoreet. Augue eget arcu dictum varius duis. Id semper risus in hendrerit. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Volutpat sed cras ornare arcu dui vivamus arcu felis bibendum. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Proin nibh nisl condimentum id venenatis. Congue mauris rhoncus aenean vel elit scelerisque mauris.";
+    "Welcome! I’m Karen. While I’ve got websites and social media stuff elsewhere, I recently decided to take on a bit of a project that I suspect will be worth documenting. So here we are. I’m not quite sure what this will be but whatever it is, I don’t think it will be boring. Wish me luck!";
   const smallText = fullText.split(" ").slice(0, 50).join(" ");
   const [open, setOpen] = useState(false);
   const [text, setText] = useState(smallText);
@@ -28,23 +28,23 @@ const About = () => {
       <Container className={styles.mainContent}>
         <ul className={styles.socialsContainer}>
           <li>
-            <a href="#">
-              <Icon as={GrPinterest} />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <Icon as={GrTwitter} />
-            </a>
-          </li>
-          <li>
-            <a href="#">
+            <a href="https://www.facebook.com/karenwrightcoach">
               <Icon as={GrFacebook} />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.instagram.com/karenwrightcoach/">
               <Icon as={GrInstagram} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.pinterest.ca/kwrightcoach/">
+              <Icon as={GrPinterest} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/kwrightcoach">
+              <Icon as={GrTwitter} />
             </a>
           </li>
         </ul>
